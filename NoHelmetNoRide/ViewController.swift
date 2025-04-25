c63cf8e893f94ba32e59253712ec7c90be247976
NH <mnh4140@gmail.com>
2025-04-25 20:11:47 +0900

    [Env] 프로젝트 초기 설정
    - 코드베이스 설정 완료
    - main.storyboard 삭제

---
+++ NoHelmetNoRide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoHelmetNoRide
+//
+//  Created by NH on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
