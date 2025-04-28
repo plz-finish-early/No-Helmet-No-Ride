9e70caeee798a010530771067fcdf672f1b3c7d2
Wonsik1 <jos505000@gmail.com>
2025-04-28 21:52:52 +0900

    [Fix]: ViewController 빼먹고 올림

---
+++ NoHelmetNoRide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoHelmetNoRide
+//
+//  Created by 전원식 on 4/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .main
+    }
+
+
+}
