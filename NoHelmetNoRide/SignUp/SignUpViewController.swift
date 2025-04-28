0d47f28e30f5acd7d14a5137c4371cbf04e7c4dd
NH <mnh4140@gmail.com>
2025-04-28 15:24:40 +0900

    [Design] #3 - 회원가입 View 완료

---
+++ NoHelmetNoRide/SignUp/SignUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.swift
+//  NoHelmetNoRide
+//
+//  Created by NH on 4/28/25.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    let signUpView = SignUpView()
+    
+    override func loadView() {
+        self.view = signUpView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
