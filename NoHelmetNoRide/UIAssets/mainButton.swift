306cf7772eb839d346c9d214de7c03a0b3da1e2a
LCH-1228 <chanho1228@gmail.com>
2025-04-28 11:07:27 +0900

    [Design] mainButton UI추가

---
+++ NoHelmetNoRide/UIAssets/mainButton.swift
@@ -0,0 +1,43 @@
+//
+//  mainButton.swift
+//  NoHelmetNoRide
+//
+//  Created by LCH on 4/28/25.
+//
+
+import UIKit
+import SnapKit
+
+class mainButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(title: String, font: UIFont, color: UIColor) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.backgroundColor = color
+        self.titleLabel?.font = font
+        setConfig() //init시 setConfig 메서드 호출
+    }
+
+    convenience init(title: String) {
+        self.init(title: title,
+                  font: .systemFont(ofSize: 24, weight: .thin),
+                  color: .main)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 인스턴스가 생성될때 초기 설정 값 설정 메서드
+    func setConfig() {
+        self.layer.cornerRadius = 20
+        self.setTitleColor(.font, for: .normal)
+        self.snp.makeConstraints {
+            $0.height.equalTo(80)
+        }
+    }
+}
