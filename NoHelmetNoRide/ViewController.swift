f88865fc280f34a46207bf6b378c4ea86cc335da
NH <mnh4140@gmail.com>
2025-04-28 13:07:48 +0900

    [Design] #2 - 로그인 View 생성

--- NoHelmetNoRide/ViewController.swift
+++ NoHelmetNoRide/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .main
+        
+        let loginVc = LoginViewController()
+        let login = UINavigationController(rootViewController: loginVc)
+        
+        setup(login)
     }
-
-
+    
+    func setup(_ child: UIViewController) {
+        addChild(child)
+        self.view.addSubview(child.view)
+        
+        child.view.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        // 앤 뭐야
+        child.didMove(toParent: self)
+    }
 }
 
