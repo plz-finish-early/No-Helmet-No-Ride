aa0254b8ef78766d7ec9e83328737b5c1aec2974
NH <mnh4140@gmail.com>
2025-04-30 19:03:38 +0900

    [Feat] #33 마이페이지 버튼 액션 개발
    - 기존 화면 리펙토링
      - 스택 자체를 버튼처럼 액션 추가
    - 버튼 액션 추가

--- NoHelmetNoRide/MyPage/MyPageViewController.swift
+++ NoHelmetNoRide/MyPage/MyPageViewController.swift
@@ -15,7 +15,62 @@ class MyPageViewController: UIViewController {
         self.view = myPageView
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addHistoryButtonAction()
+        addStatusButtonAction()
+        addKickboardButtonAction()
+        addLogoutButtonAction()
+    }
+    
+    func addHistoryButtonAction() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTappedHistoryButton))
+        myPageView.historyStack.addGestureRecognizer(tap)
+    }
+    
+    // 새로 추가
+    @objc func didTappedHistoryButton() {
+        let usageHistoryViewController = UsageHistoryViewController()
+        navigationController?.pushViewController(usageHistoryViewController, animated: true)
+    }
+    
+    func addStatusButtonAction() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTappedStatusButton))
+        myPageView.statusStack.addGestureRecognizer(tap)
+    }
+    
+    // 새로 추가
+    @objc func didTappedStatusButton() {
+        let usageStatusViewController = UsageStatusViewController(status: .using)
+        navigationController?.pushViewController(usageStatusViewController, animated: true)
+    }
+    
+    func addKickboardButtonAction() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTappedKickboardButton))
+        myPageView.kickboardStack.addGestureRecognizer(tap)
+    }
+    
+    // 새로 추가
+    @objc func didTappedKickboardButton() {
+        let registeredKickboardViewController = RegisteredKickboardViewController()
+        navigationController?.pushViewController(registeredKickboardViewController, animated: true)
+    }
+    
+    func addLogoutButtonAction() {
+        myPageView.logoutButton.addTarget(self, action: #selector(didTappedLogoutButton), for: .touchUpInside)
+    }
+    
+    // 새로 추가
+    @objc func didTappedLogoutButton() {
+        //let vc = LoginViewController()
+        //navigationController?.pushViewController(vc, animated: true)
+        navigationController?.popViewController(animated: true)
     }
 }
