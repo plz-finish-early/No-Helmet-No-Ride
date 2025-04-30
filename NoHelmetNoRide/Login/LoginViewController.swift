ca86237b5b9bec4a60f5fc2ceabcd6fb1291f1d3
NH <mnh4140@gmail.com>
2025-04-30 12:09:37 +0900

    [Feat] #33 - 로그인 View 테스트용 로그인 버튼 액션 개발
    - 버튼을 탭하면 알림창을 띄웁니다.
    - 아직 기능개발이 다 되지 않아, 로그인 실패 알림창만 띄웁니다.

--- NoHelmetNoRide/Login/LoginViewController.swift
+++ NoHelmetNoRide/Login/LoginViewController.swift
@@ -24,6 +24,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         addSignUpButtonAction() // 회원가입 버튼 액션 추가
+        addLoginButtonAction() // 로그인 버튼 액션 추가
     }
     
     // 회원가입 버튼 액션 추가
@@ -36,6 +37,25 @@ class LoginViewController: UIViewController {
         let signUpVC = SignUpViewController() // 모달로 띄울 뷰 컨트롤러 설정
         self.presentModal(signUpVC)
     }
+    
+    // 로그인 버튼 액션 추가
+    private func addLoginButtonAction() {
+        loginView.mainButton.addTarget(self, action: #selector(didTappedLoginButtonButton), for: .touchUpInside)
+    }
+    
+    @objc
+    private func didTappedLoginButtonButton() {
+        // TODO: 코어 데이터에 아이디 비밀번호 값 맞는지 확인하는 로직
+        
+        // TODO: 로그인 데이터가 맞으면 지도 화면으로 이동
+        
+        // TODO: 로그인 데이터가 맞지 않다면, 알림창 띄우기
+        let alert = UIAlertController(title: "로그인 실패", message: "로그인에 실패하였습니다.\n아이디와 비밀번호를 확인해주세요.", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
